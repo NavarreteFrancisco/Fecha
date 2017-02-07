762587eaf3aa60378d5625306b80b7893ae31084
admin <admin@w701>
2017-02-07 16:03:07 +0100

    Primera version sin refactorizar

--- src/Fecha.java
+++ src/Fecha.java
@@ -2,7 +2,12 @@ public class Fecha {
 		private int dia;
 		private int mes;
 		private int anio;
-
+		
+		public Fecha(int dia, int mes, int anio) {
+			this.dia = dia;
+			this.mes = mes;
+			this.anio = anio;
+		}
 		public boolean valida() {
 			if (dia < 1 || dia > 31)
 				return false;
